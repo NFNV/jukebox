7f63ea0f9ce28945c3b5d227292de6bbcf88a27c
Nicolás Valiconto <nvaliconto@outlook.com>
2023-10-18 18:02:40 -0300

    serializer and views ready, info displayed

--- api/models.py
+++ api/models.py
@@ -1,4 +1,15 @@
 from django.db import models
+import random
+import string
+
+def generate_unique_code():
+    length = 6
+
+    while True:
+        code = ''.join(random.choice(string.ascii_uppercase, k=length))
+        if Room.objects.filter(code=code).count() == 0:
+            break
+    return code
 
 # Create your models here.
 class Room(models.Model):

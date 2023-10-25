ce74b9df2c36494460adef0751c86bd2b3afa4dd
Nicolás Valiconto <nvaliconto@outlook.com>
2023-10-25 16:54:00 -0300

    App.js fixed

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,15 +1,9 @@
-import React, { Component } from "react"
-import { render } from "react-dom"
+import React from "react";
+import { render } from "react-dom";
 
-export default class App extends Component {
-    constructor(props) {
-        super(props)
-    }
-
-    render(){
-        return (<h1>Test</h1>)
-    }
+function App() {
+  return <h1>Test</h1>;
 }
 
-const appDiv = document.getElementById("app")
-render(<App />, appDiv)
+const appDiv = document.getElementById("app");
+render(<App />, appDiv);
